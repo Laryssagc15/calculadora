21ff0cb1a09f96fb2dee8f665e55554b5573b4a1
Laryssagc15 <51210345+Laryssagc15@users.noreply.github.com>
2019-08-09 17:17:57 -0300

    Add files via upload

---
+++ build/precache-manifest.5239642208c60f463c2751f76ef49a12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581aa1c613c0900da8e2210dbd91c84e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70383a9cb7bce8da4719",
+    "url": "/static/css/main.ec9784cb.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "70383a9cb7bce8da4719",
+    "url": "/static/js/main.5ab467a3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7b9659dca0b0095eb1e074c98accda58",
+    "url": "/static/media/Hind-Medium.7b9659dc.ttf"
+  }
+]);
